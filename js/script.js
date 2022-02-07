85f51281eb5f43436cf8f09e69fd5f694d42b71d
Sol Youn <syoun93@gmail.com>
2022-02-07 11:55:17 -0800

    updated js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ console.log("Loona")
 const gameSpace = document.querySelector(".container")
 const displayGuesses = document.querySelector("#guess")
 
+
+chosenCard = []
+
+// Start gueesses at 0 and increment them as number of clicks go up
+var guesses = 0
+displayGuesses.innerText = guesses
+
 // Create an array for all member's images 12 members and then repeat for 24 images
-const getImages = () => [
+const getImages = [
   { imgSrc: './images/Loona-Heejin.jpg', name: 'Loona Heejin'},
   { imgSrc: './images/Loona-Hyunjin.jpg', name: 'Loona Hyunjin'},
   { imgSrc: './images/Loona-Haseul.jpg', name: 'Loona Haseul'},
@@ -29,44 +36,53 @@ const getImages = () => [
   { imgSrc: './images/Loona-GoWon.jpg', name: 'Loona GoWon'},
   { imgSrc: './images/Loona-OliviaHye.jpg', name: 'Loona OliviaHye'}
 ]
+// Randomize the images
+getImages.sort( () => Math.random()- 0.5)
 
 
 // Function to create the squares using a loop
 // make the divs into ids instead of making them imgs... 
 let makeGameSpace = () => {
-  for (let i = 0; i < 24; i++) {
-    let square = document.createElement("img")
-    square.classList.add("square")
-    square.setAttribute('src', 'images/Loona-Logo.jpg')
-    square.setAttribute("id", 'square' + i)
-    square.innerText = i
-    gameSpace.appendChild(square)
+  for (let i = 0; i < getImages.length; i++) {
+    let cards = document.createElement("img")
+    cards.classList.add("card")
+    cards.setAttribute('src', 'images/Loona-Logo.jpg')
+    cards.setAttribute("id", 'card' + i)
+    cards.addEventListener('click' , revealCard)
+    gameSpace.appendChild(cards)
   }
 }
 
+let revealCard = () => {
+  const cardSelector = Array.from(document.querySelectorAll('.card'))
+  cardSelector.forEach((card, index) => {
+    card.setAttribute('src', getImages[index].imgSrc)
+  })
+  } 
 
-// Function to shuffle  Fisher-Yates Shuffle
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex
+  
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex--
+// // Function to shuffle  Fisher-Yates Shuffle
+// function shuffle(array) {
+//   let currentIndex = array.length,
+//     randomIndex
 
-    // And swap it with the current element.
-    ;[array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ]
-  }
-  return array
-}
+//   // While there remain elements to shuffle...
+//   while (currentIndex != 0) {
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex)
+//     currentIndex--
+
+//     // And swap it with the current element.
+//     ;[array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex],
+//       array[currentIndex],
+//     ]
+//   }
+//   return array
+// }
 
 
 document.addEventListener("DOMContentLoaded", () => {
   makeGameSpace()
-  console.log(makeGameSpace)
 })
