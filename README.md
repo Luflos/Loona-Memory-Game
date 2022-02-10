6a58d3c8e9014a51df9a14605c5d60cabc9d47a9
Sol Youn <syoun93@gmail.com>
2022-02-10 13:51:57 -0800

    added music + edited readme

--- README.md
+++ README.md
@@ -7,26 +7,27 @@ You might be asking yourself, what is LOONA? LOONA is you, it's me but it's espe
 ### Tech Being Used
 * HTML/CSS/JavaScript
 * Using grid and flex
-* Possibly using bootstrap to style page
 
-### Rough Sketch
-![Sketch](./images/MemoryGameSketch.jpg)
-Beautiful right? Please excuse my drawing...
+
+### Game Board
+![Game Board](./images/Loona-Memory-Game.jpg)
 
 
 ## MVP
-
 * Create a 4x6 grid that contains images
 * Have a score (# of guesses), and a reset button
-* Have the images be started face down and be flipped when they are pressed
-* Have only two images be flipped at any given time
-* At the start and when the reset button is pressed, have the images be shuffled
+* Have the images be started with the Loona logo and turn into a member once a logo is clicked.
+* Have only two images be present at any given time
+* At the start of the game and when the reset button is pressed, have the images be shuffled
 * Have instructions somewhere on the page
 * Have a backdrop of LOONA (possibly album cover)
-* Game state where the game ends when all images have been matched
+* Game ends when all the images are matched and "You Win!" will appear at the top.
 
 ## Stretch Goals
-* Whenever a user gets a match facts about that member will be displayed
-* Behind each image have a glowing backdrop of that members representative color
-* Have sound effects whenever an image is flipped
-* A timer to score instead of # of guesses and have timer stop when game is over
+* Whenever a user gets a match facts about that member will be displayed.
+* Behind each image have a glowing backdrop of that members representative color.
+* Have a button that plays a LOONA song. The button can toggle the music on and off.
+* A timer to score instead of # of guesses and have timer stop when game is over.
+
+## Resources
+* Used this page to find how to properly play and pause music. <a href="https://forum.freecodecamp.org/t/how-to-play-mp3-in-the-background-music-automatically/308554l">Click</a>
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const displayGuesses = document.querySelector("#guess");
 const resetButton = document.querySelector("#resetButton");
 const winMessage = document.querySelector("#winMessage");
 const extraMessage = document.querySelector("#extra");
+const instructions = document.querySelector('.instructions')
 
 // Start gueesses at 0 and increment them as number of clicks go up
 let guesses = 0;
@@ -60,6 +61,7 @@ let clickedElement = null;
 let pause = false;
 
 // function to change the card
+// pause so the user cannot click on more than two images
 let revealCard = (event) => {
   if (pause) {
     return;
@@ -102,64 +104,65 @@ let revealCard = (event) => {
 // Win Condition - Counter = 12 or if there are all matches print out a message
 let winCondition = () => {
   if (correctGuesses >= 12) {
-  winMessage.innerText = "You Win";
-  setTimeout(() => {
-    extraMessage.style.color= 'black';
-    extraMessage.innerText = "LOONA is Life";
-  }, 4000)
-}
+    winMessage.innerText = "You Win!";
+    setTimeout(() => {
+      // document.body.style.backgroundImage = "url("../images/LOONAVERSE.jpg")";
+      extraMessage.style.color = "black";
+      extraMessage.innerText = "Welcome to the LOONAVERSE";
+    }, 4000);
+  }
 };
 
 // Extra Goal
 let getFact = (name) => {
   if (name === "Loona Heejin") {
-    extraMessage.style.color= '#F50B94';
+    extraMessage.style.color = "#F50B94";
     extraMessage.innerText =
-      "HeeJin was the 1st member revealed. Her represetative animal is a Rabbit🐇 and her color is Vivid Pink";
+      "HeeJin was the 1st member introduced. Her represetative animal is a Rabbit🐇 and her color is Vivid Pink";
   } else if (name === "Loona Hyunjin") {
-    extraMessage.style.color= '#FCCA28';
+    extraMessage.style.color = "#FCCA28";
     extraMessage.innerText =
-      "HyunJin was the 2nd member revealed. Her represetative animal is a Cat🐈 and her color is Yellow";
+      "HyunJin was the 2nd member introduced. Her represetative animal is a Cat🐈 and her color is Yellow";
   } else if (name === "Loona Haseul") {
-    extraMessage.style.color= '#08A953';
+    extraMessage.style.color = "#08A953";
     extraMessage.innerText =
-      "HaSeul was the 3rd member revealed. Her represetative animal is a White Bird🕊️ and her color is Green";
+      "HaSeul was the 3rd member introduced. Her represetative animal is a White Bird🕊️ and her color is Green";
   } else if (name === "Loona Yeojin") {
-    extraMessage.style.color= '#F5712A';
+    extraMessage.style.color = "#F5712A";
     extraMessage.innerText =
-      "YeoJin was the 4th member revealed. Her represetative animal is a Frog🐸 and her color is Orange";
+      "YeoJin was the 4th member introduced. Her represetative animal is a Frog🐸 and her color is Orange";
   } else if (name === "Loona Vivi") {
-    extraMessage.style.color= '#F59EAF';
+    extraMessage.style.color = "#F59EAF";
     extraMessage.innerText =
-      "ViVi was the 5th member revealed. Her represetative animal is a Deer🦌 and her color is Light Pink";
+      "ViVi was the 5th member introduced. Her represetative animal is a Deer🦌 and her color is Light Pink";
   } else if (name === "Loona KimLip") {
-    extraMessage.style.color= '#EE1B3E';
+    extraMessage.style.color = "#EE1B3E";
     extraMessage.innerText =
-      "KimLip was the 6th member revealed. Her represetative animal is an Owl🦉 and her color is Red";
+      "KimLip was the 6th member introduced. Her represetative animal is an Owl🦉 and her color is Red";
   } else if (name === "Loona JinSoul") {
-    extraMessage.style.color= '#1E76B8';
+    extraMessage.style.color = "#1E76B8";
     extraMessage.innerText =
-      "JinSoul was the 7th member revealed. Her represetative animal is a Blue Betta🐟 and her color is Blue";
+      "JinSoul was the 7th member introduced. Her represetative animal is a Blue Betta🐟 and her color is Blue";
   } else if (name === "Loona Choerry") {
-    extraMessage.style.color= '#7C2E87';
+    extraMessage.style.color = "#7C2E87";
     extraMessage.innerText =
-      "Choerry was the 8th member revealed. Her represetative animal is a Fruit Bat🦇 and her color is Purple";
+      "Choerry was the 8th member introduced. Her represetative animal is a Fruit Bat🦇 and her color is Purple";
   } else if (name === "Loona Yves") {
-    extraMessage.style.color= '#7A0434';
+    extraMessage.style.color = "#7A0434";
     extraMessage.innerText =
-      "Yves was the 9th member revealed. Her represetative animal is a Swan🦢 and her color is Burgundy";
+      "Yves was the 9th member introduced. Her represetative animal is a Swan🦢 and her color is Burgundy";
   } else if (name === "Loona Chuu") {
-    extraMessage.style.color= '#F98F81';
+    extraMessage.style.color = "#F98F81";
     extraMessage.innerText =
-      "Chuu was the 10th member revealed. Her represetative animal is a Penguin🐧 and her color is Peach";
+      "Chuu was the 10th member introduced. Her represetative animal is a Penguin🐧 and her color is Peach";
   } else if (name === "Loona GoWon") {
-    extraMessage.style.color= '#37BA9B';
+    extraMessage.style.color = "#37BA9B";
     extraMessage.innerText =
-      "GoWon was the 11th member revealed. Her represetative animal is an Emperor Butterfly🦋 and her color is Eden Green";
+      "GoWon was the 11th member introduced. Her represetative animal is an Emperor Butterfly🦋 and her color is Eden Green";
   } else if (name === "Loona OliviaHye") {
-    extraMessage.style.color= '#C0C2C2';
+    extraMessage.style.color = "#C0C2C2";
     extraMessage.innerText =
-      "Olivia Hye was the 12th & final member revealed. Her represetative animal is a Wolf🐺 and her color is Silver";
+      "Olivia Hye was the 12th & final member introduced. Her represetative animal is a Wolf🐺 and her color is Silver";
   }
 };
 
@@ -174,6 +177,19 @@ resetButton.addEventListener("click", function () {
   clickedElement = null;
   pause = false;
   makeGameSpace();
+});
+
+// Music button
+const musicbutton = document.querySelector("#musicButton");
+const audio = document.querySelector("audio");
+
+musicButton.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+  } else {
+    audio.pause();
+  }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
